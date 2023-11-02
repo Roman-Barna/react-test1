2f9ac66870d4470d68275afedeef1c3f347577ff
Roman Barna <frontenddeveloperbarna@gmail.com>
2023-11-03 01:58:59 +0200

    change api

--- src/api/routeApi.tsx
+++ src/api/routeApi.tsx
@@ -2,6 +2,6 @@ import axios from 'axios';
 import { DataApiRoute } from '../App';
 
 export const getAllRoute = async () => {
-   return await axios.get(`/test-json/testApi.json`)
+   return await axios.get(`./react-test1/test-json/testApi.json`)
       .then(res => res.data as DataApiRoute[])
 }
